267ec70c1795857a0d675936ddf92f75db3cbe05
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-10-02 02:06:00 +0900

    [WIP] basic implementation

--- argparse.hpp
+++ argparse.hpp
@@ -1,104 +1,90 @@
-///
-/// Copyright (c) 2015 Yusuke Sasaki
-///
-/// This software is released under the MIT License.
-/// See http://opensource.org/licenses/mit-license.php
-/// or <LICENSE>.
-///
-
 #pragma once
 
-#include <algorithm>
 #include <string>
 #include <vector>
 #include <tuple>
 
-
 namespace argparse {
 
 template <typename T>
-class default_reader {
-  public:
-    bool operator()(T const&) const { return true; }
+struct argument {
+   using arg_type = T;
+
+   argument(std::string const& name,
+            char short_name,
+            std::string const& help)
+      : name_(name)
+      , short_name_(short_name)
+      , help_(help)
+   {
+   }
+
+   std::string const& name() const {
+      return name_;
+   }
+
+   bool has_short_name() const {
+      return short_name_ != '\0';
+   }
+
+   char short_name() const {
+      if (!has_short_name())
+         throw std::logic_error("no short name");
+      return short_name_;
+   }
+
+   std::string const& help() const {
+      return help_;
+   }
+
+private:
+   std::string name_;
+   char short_name_;
+   std::string help_;
 };
 
 template <typename T>
-class oneof_reader {
-    std::vector<T> vals;
-  public:
-    oneof_reader(std::initializer_list<T> v) : vals(v) {}
-    bool operator()(T const& v) const {
-        return std::find(vals.begin(), vals.end(), v) != vals.end();
-    }
-};
-
-template <typename... Args>
-oneof_reader<typename std::common_type<Args...>::type> oneof(Args... args) {
-    return oneof_reader<typename std::common_type<Args...>::type>{ std::forward<Args>(args)... };
-}
-
-template <typename T, class = typename std::enable_if<std::is_arithmetic<T>::value>::type>
-class range_reader {
-    T low, high;
-  public:
-    range_reader(T low, T high) : low(low), high(high) {}
-
-    bool operator()(T const& v) const { return low <= v && v < high; }
-};
-
-template <typename T>
-range_reader<T> range(T low, T high) {
-    return range_reader<T>{low, high};
-}
-
-template <typename T, typename Reader = default_reader<T>>
-class argument {
-  public:
-    using arg_type = T;
-    argument(std::string const& name, std::string const& description, Reader reader = Reader())
-        : name(name), description(description), reader(reader) {}
-  private:
-    std::string name;
-    std::string description;
-    Reader reader;
-};
-
-template <typename T>
-argument<T> arg(std::string const& name, std::string const& description) {
-    return argument<T>(name, description);
+inline argument<T> arg(std::string const& name,
+                char short_name = '\0',
+                std::string const& help = "")
+{
+   return argument<T>{ name, short_name, help };
 }
 
 template <typename T>
-argument<T, range_reader<T>> arg(std::string const& name, std::string const& description,
-                                 range_reader<T> reader) {
-    return argument<T, range_reader<T>>(name, description);
-}
-
-template <typename T>
-argument<T, oneof_reader<T>> arg(std::string const& name, std::string const& description,
-                                 oneof_reader<T> reader) {
-    return argument<T, oneof_reader<T>>(name, description, reader);
+inline argument<T> arg(std::string const& name,
+                std::string const& help)
+{
+   return argument<T>{ name, '\0', help };
 }
 
 template <typename... Args>
-class parser {
-  public:
-    using args_type = std::tuple<typename Args::arg_type...>;
-    parser(Args&&... args): arguments{std::forward<Args>(args)...} {}
-    void parse(int argc, char const* argv[]) {}
-    std::tuple<args_type, std::vector<std::string>> parsed() const {
-        return std::tuple<args_type, std::vector<std::string>>();
-    }
-  private:
-    std::tuple<Args...> arguments;
-    args_type parsed_args;
-    std::vector<std::string> remains_;
-    std::string prog_name;
+struct parser
+{
+   parser(Args&&... args)
+      : args_{ std::forward<Args>(args)... }
+   {
+   }
+
+   void parse(std::vector<std::string> args) {
+      if (args.size() == 0)
+         throw std::runtime_error("argument must be at least one item(s).");
+
+      prog_name_ = args[0];
+      remains_.assign(args.begin()@+1, args.end());
+   }
+
+private:
+   std::string prog_name_;
+   std::tuple<typename Args::arg_type...> options_;
+   std::vector<std::string> remains_;
+
+   std::tuple<Args...> args_;
 };
 
 template <typename... Args>
 parser<Args...> make_parser(Args&&... args) {
-    return parser<Args...>{std::forward<Args>(args)...};
+   return parser<Args...>{ std::forward<Args>(args)... };
 }
 
 } // namespace argparse;
--- main.cpp
+++ main.cpp
@@ -1,19 +1,56 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <type_traits>
+#include <string>
+using namespace std;
+
+#define STATIC_ASSERT_EQ(expected, actual) \
+   static_assert(std::is_same<expected, actual>::value, #expected "!=" #actual)
+
 #include "argparse.hpp"
-#include <iostream>
 
-int main(int argc, char const* argv[])
+
+TEST_CASE("regular use", "[argument]")
 {
-    auto parser = argparse::make_parser(
-        argparse::arg<bool>("--hoge", "some argument"),
-        argparse::arg<int> ("--fuga", "some argument")
-    );
+   argparse::argument<int> a = argparse::arg<int>("hoge", 'h', "short description");
 
-    parser.parse(argc, argv);
+   STATIC_ASSERT_EQ(decltype(a)::arg_type, int);
 
-    decltype(parser)::args_type args;
-    std::vector<std::string> remains;
-    std::tie(args, remains) = parser.parsed();
+   REQUIRE(a.name() == "hoge");
+   REQUIRE(a.has_short_name());
+   REQUIRE(a.short_name() == 'h');
+   REQUIRE(a.help() == "short description");
+}
 
-    argparse::range_reader<float> range(0, 10);
-    std::cout << std::boolalpha << range(10) << std::endl;
+TEST_CASE("omit short_name", "[argument]")
+{
+   argparse::argument<int> a = argparse::arg<int>("hoge", "desc");
+
+   REQUIRE(!a.has_short_name());
+   REQUIRE_THROWS(a.short_name());
+}
+
+
+TEST_CASE("make parser", "[parser]")
+{
+   argparse::parser<argparse::argument<int>, argparse::argument<string>>
+      p = argparse::make_parser(
+         argparse::arg<int>("hoge"),
+         argparse::arg<string>("fuga", 'f')
+      );
+
+   SECTION("parse") {
+      p.parse({ "progname", "--hoge", "10", "aaa", "-f", "foo", "bb", "cc" });
+
+      REQUIRE(p.progname() == "progname");
+
+      tuple<int, string> const& options = p.options();
+      REQUIRE(std::get<0>(options) == 10);
+      REQUIRE(std::get<1>(options) == "foo");
+
+      vector<string> const& remains = p.remains();
+      REQUIRE(remains[0] == "aaa");
+      REQUIRE(remains[1] == "bb");
+      REQUIRE(remains[2] == "cc");
+   }
 }
