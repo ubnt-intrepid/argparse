bf7b642a7e0418773d57a99d14c89bce47989289
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-10-02 18:11:48 +0900

    add lexical_cast()

--- argparse.hpp
+++ argparse.hpp
@@ -3,8 +3,69 @@
 #include <string>
 #include <vector>
 #include <tuple>
+#include <sstream>
+#include <type_traits>
+
 
 namespace argparse {
+
+namespace detail {
+    template <typename In, typename Out, bool pass = std::is_convertible<In, Out>::value>
+    struct lexical_cast_t;
+
+    template <typename In, typename Out>
+    struct lexical_cast_t<In, Out, true> {
+        static Out apply(In const& val) {
+            return static_cast<Out>(val);
+        }
+    };
+
+    template <typename In>
+    struct lexical_cast_t<In, std::string, false> {
+        static std::string apply(In const& val) {
+            std::ostringstream osstr;
+            osstr << val;
+            return osstr.str();
+        }
+    };
+
+    template <typename Out>
+    struct lexical_cast_t<std::string, Out, false> {
+        static Out apply(std::string const& val) {
+            std::istringstream isstr(val);
+            Out out;
+            isstr >> out;
+            return out;
+        }
+    };
+
+    template <typename Out, std::size_t N>
+    struct lexical_cast_t<char[N], Out, false> {
+        static Out apply(char const (&val)[N]) {
+            std::istringstream isstr(val);
+            Out out;
+            isstr >> out;
+            return out;
+        }
+    };
+
+    template <typename Out, std::size_t N>
+    struct lexical_cast_t<char const[N], Out, false> {
+        static Out apply(char const (&val)[N]) {
+            std::istringstream isstr(val);
+            Out out;
+            isstr >> out;
+            return out;
+        }
+    };
+} // namespace detail;
+
+template <typename Out, typename In>
+inline Out lexical_cast(In const& val) {
+    return detail::lexical_cast_t<In, Out>::apply(val);
+}
+
+//
 
 template <typename T>
 struct argument {
