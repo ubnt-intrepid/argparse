4f4ce2d7835831754d343c8602435eb92bf8dcab
Yusuke Sasaki <y_sasaki@nuem.nagoya-u.ac.jp>
2015-10-04 00:46:05 +0900

    fix test

--- main.cpp
+++ main.cpp
@@ -12,23 +12,27 @@ using namespace std;
 TEST_CASE("make parser", "[parser]")
 {
    using argparse::arg;
+   using argparse::flag;
 
    using parser_t = argparse::parser<
       argparse::argument_with_value<int>,
-      argparse::argument_with_value<string>
+      argparse::argument_with_value<string>,
+      argparse::argument_flag
    >;
    parser_t p = argparse::make_parser(
       arg<int>   ("hoge"),
-      arg<string>("fuga", 'f')
+      arg<string>("fuga", 'f'),
+      flag("foo")
    );
 
-   p.parse({ "progname", "--hoge", "10", "aaa", "-f", "foo", "bb", "cc" });
+   p.parse({ "progname", "--hoge", "10", "aaa", "-f", "foo", "bb", "cc", "--foo" });
 
    REQUIRE(p.progname() == "progname");
 
-   tuple<int, string> const& options = p.options();
+   tuple<int, string, bool> const& options = p.options();
    REQUIRE(std::get<0>(options) == 10);
    REQUIRE(std::get<1>(options) == "foo");
+   REQUIRE(std::get<2>(options) == true);
 
    vector<string> const& remains = p.remains();
    REQUIRE(remains[0] == "aaa");
